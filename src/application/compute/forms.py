da8e16fe7b2fc5654f9a05be39fa6543a28bc002
Prashant Khanduri <pkhanduri@jawbone.com>
2015-02-18 23:45:14 -0800

    Removing unwanted files from the base project

--- src/application/compute/forms.py
+++
@@ -1,7 +0,0 @@
-import flask_wtf
-import wtforms
-
-
-class ComputeForm(flask_wtf.Form):
-    a = wtforms.IntegerField(label='a', validators=[wtforms.validators.DataRequired()])
-    b = wtforms.IntegerField(label='b', validators=[wtforms.validators.DataRequired()])
